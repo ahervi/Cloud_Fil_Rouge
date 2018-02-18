8834e2893890069bb6d9cb4b2ad3210b7366aae3
ahervi <hervieu.antoine@gmail.com>
2018-02-18 15:42:41 +0100

    Ajout d'un début de ui mais problème de type CORS quand on envoiue la requête au microservice photographer

--- src/ms/ui/app.py
+++ src/ms/ui/app.py
@@ -1,11 +1,31 @@
-#!/bin/python
+#!/usr/bin/env python
 from flask import Flask
+import connexion
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+ip_addr = "127.0.O.1"
+photographers_app_port = "8090"
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def index():
-    return "Hello, World!"
+
+	res = "Define photographer:<br/>"
+	res += "<form id='photographer_form' method='post' action='http://127.0.0.1:8090/photographers'>"
+	res += "First name:<br/><input type='test' name='first_name' id='first_name_input'><br/>"
+	res += "Display name:<br/><input type='test'name='display_name' id='display_name_input'><br/>"
+	res += "Interests:<br/><input name='interests' id='interests_input_1'><br/>"
+	res += "<input name='interests' id='interests_input_2'><br/>"
+	res += "<input name='interests' id='interests_input_3'><br/>"
+	res += "Last name:<br/><input type='test'name='last_name' d='last_name_input'><br/><br/>"
+	res += "<input type='submit' value='Submit' id='photographer_form_submit'>"
+	res += "</form>"
+	res += "<script type=\"text/javascript\" src=static/input2json.js></script>"
+	return res
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/ms/ui/static/input2json.js
@@ -0,0 +1,31 @@
+var form = document.getElementById('photographer_form')
+form.onsubmit = function (e) {
+  // stop the regular form submission
+  e.preventDefault();
+    // collect the form data while iterating over the inputs
+  var data = {};
+  for (var i = 0, ii = form.length; i < ii; ++i) {
+    var input = form[i];
+    if (input.name == "interests") {
+      var input2 = form[i+1];
+      var input3 = form[i+2];
+    	data[input.name] = [input.value, input2.value, input3.value];
+    	i+=2;
+    }
+    else {
+    	data[input.name] = input.value;
+    }
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open(form.method, form.action, true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+	alert(JSON.stringify(data));
+ // send the collected data as JSON
+  xhr.send(JSON.stringify(data));
+
+  xhr.onloadend = function () {
+    // done
+  };
+};
